31cc83c26a78763d4f6b88da6c0af2738ade60b6
Budhi Octaviansyah <budioct@gmail.com>
2024-12-16 17:32:49 +0700

    add configuration-server mirco-service

---
+++ config-server/src/test/java/budhioct/dev/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package budhioct.dev.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
